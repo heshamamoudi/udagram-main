806ad99cc4806eba7c13c18a55af01196e40c2ae
heshamamoudi <hesham69mez@gmail.com>
2022-09-11 02:09:30 +0300

    1

--- udagram-main/udagram/udagram-frontend/src/environments/environment.ts
+++ udagram-main/udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udacity-full.eba-a3ndeqsm.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udacity-full2.eba-utisqth6.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
