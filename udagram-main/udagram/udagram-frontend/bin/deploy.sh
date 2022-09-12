36519b9c2ed8a9cb95f167f8f24c2707f2f6bc79
heshamamoudi <hesham69mez@gmail.com>
2022-09-13 01:36:29 +0300

    d

--- udagram-main/udagram/udagram-frontend/bin/deploy.sh
+++ udagram-main/udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,3 @@
 aws s3 cp --recursive --acl public-read ./www s3://hesham-bucket/
 aws s3 cp --acl public-read-write --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://hesham-bucket/
+
