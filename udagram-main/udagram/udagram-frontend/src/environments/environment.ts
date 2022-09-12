b6a0d4ecd334aebcf3543fee721a5183c87b4094
heshamamoudi <hesham69mez@gmail.com>
2022-09-13 01:15:02 +0300

    successful backend issue was from AWS

--- udagram-main/udagram/udagram-frontend/src/environments/environment.ts
+++ udagram-main/udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-api-dev.eba-3g7r3qav.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://udagram-api-dev.eba-hh3hyawe.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
